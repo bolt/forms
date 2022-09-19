ac1aa96b8613662773db5daefc76ef9cdca1b563
Jean Fradet <jeanfradet@Jeans-MacBook-Pro.local>
2022-09-18 21:25:23 -0400

    Modified global_aliases to be stored in default bolt forms config

--- src/BoltFormsConfig.php
+++ src/BoltFormsConfig.php
@@ -73,16 +73,27 @@ class BoltFormsConfig
         $boltFormsRoot = explode('.yaml', $mainConfigPath)[0];
         $extrasConfigPath = $boltFormsRoot . DIRECTORY_SEPARATOR;
         
-        $global_aliases = file_get_contents($boltFormsRoot.'-global.yaml');
+        // Share global_aliases with other configs
+        $defaultConfig;
+        $global_aliases;
         
-        if (is_null($global_aliases) ) {$global_aliases = '';}
+        try {
+            $defaultConfig  = file_get_contents($boltFormsRoot.'.yaml');
+        }
+        catch (ParseException $e) {
+            throw new ParseException(sprintf('Default BoltForm Config Not Found'));
+        }
+        
+        $global_aliases = $this->getFormGlobals($defaultConfig);        
+        
         $finder = new Finder();
-    
         $finder->files()->in($extrasConfigPath)->name('*.yaml');
-    
+        
+        // Exit if no sub_files str
         if (! $finder->hasResults()) {
             return [];
         }
+        
         $result = [];
     
         foreach ($finder as $file) {
@@ -97,11 +108,42 @@ class BoltFormsConfig
             } catch (ParseException $e) {
                 $e;
                 throw new ParseException(sprintf('Error detected on form %s: %s', $formName, $e->getMessage()));
-            }
-            
+            } 
             unset($result[$formName]['global_aliases']);
         }
-    
+        
         return $result;
+    }
+    
+    private function getFormGlobals(string $defaultFormContents): string
+    {
+        $start = strpos($defaultFormContents, 'global_aliases');
+        
+        if ($start === false) return '';
+        
+        $end;
+        $commented_out;
+        
+        //Check if commented out
+        if ($start === 0):
+            $commented_out = false;
+        else:
+            // Check to see if a disabled through commenting (previous 5 char)
+            $rewind_check_start_pos = ($start > 5)? $start - 5 : 0;
+            $scan_str = substr($defaultFormContents,$rewind_check_start_pos, $start);
+            
+            $comment_found = strpos($scan_str, '#'.-1); //Note: actually a position, not a bool, reverse searched
+            $commented_out = ($comment_found !== false && $comment_found < strpos($scan_str, "\n".-1)); //ie: # found and no inbetween \n
+        endif;
+        
+        if ($commented_out) return '';
+        
+        // This is used to find the first instance of a line which starts with neither a whitespace or comment (ie the end of the global aliases, if starting just after the g (start + 1))
+        preg_match("/^[^\s\#]/m", $defaultFormContents."", $matches, PREG_OFFSET_CAPTURE, $start+1);
+        if (sizeof($matches) == 0) return '';
+        
+        $end = $matches[0][1];
+        $rt_str = substr($defaultFormContents,$start,$end);
+        return $rt_str;      
     }
 }
